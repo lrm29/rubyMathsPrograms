f22f6bbca377a9925a86791149d0cfe1b510909d
Laurence R. McGlashan <lrm29@cam.ac.uk>
2011-03-05 12:37:07 -0800

    Edited matrixDecomposition/matrix_decomposition_executable.rb via GitHub

--- matrixDecomposition/matrix_decomposition_executable.rb
+++ matrixDecomposition/matrix_decomposition_executable.rb
@@ -1,21 +1,42 @@
 require 'matrix.rb'
 
+class Matrix
+
+  # Assign the given value to the slot at the specified row and column
+  def []=(row, column, value)
+    @rows[row][column] = value
+  end
+
+  def to_s
+    [sprintf("%d x %d Matrix", @rows.length, column_size),
+      @rows.map { |row| row.inspect }].flatten.join("\n")
+  end
+
+end
+
 matrix = Matrix[[1, 1, 1],[1, 1, 3],[1, 3, 5]]
+L = Matrix.zero(3)
+U = Matrix.zero(3)
 
 puts "Input Matrix:"
 puts matrix
 
-n = (matrix.row_size())
+n = (matrix.row_size())-1
 
 for k in 0..(n-1)
     for x in (k+1)..n
-        matrix[x,k] = matrix[x,k] / matrix[k,k]
+        #L[x,k]=matrix[x,k]/matrix[k,k]
+        matrix[x,k]=matrix[x,k]/matrix[k,k]
     end
     for i in (k+1)..n
         for j in (k+1)..n
-            matrix[i,k] = matrix[i,j] - matrix[i,k]*[k,j]
+            #U[i,k]=matrix[i,j]-matrix[i,k]*matrix[k,j]
+            matrix[i,k]=matrix[i,j]-matrix[i,k]*matrix[k,j]
         end
     end
 end
 
 puts matrix
+
+print "\nHit <Enter> to Exit."
+STDIN.gets
